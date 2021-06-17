0d3a9e334a9e57740f2b54a695d9593f9bd46495
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2021-06-17 03:17:15 +0200

    update test file

--- 7 kyu/descending-order.test.ts
+++ 7 kyu/descending-order.test.ts
@@ -1,11 +1,13 @@
 import { descendingOrder } from './descending-order';
 
 test('should return some sample numbers in descending order', () => {
-  [
+  const fixtures: [number, number][] = [
     [42145, 54421],
     [145263, 654321],
     [123456789, 987654321],
-  ].forEach(([n1, n2]) => {
-    expect(descendingOrder(n1!)).toEqual(n2!);
+  ];
+
+  fixtures.forEach(([n1, n2]) => {
+    expect(descendingOrder(n1)).toEqual(n2);
   });
 });

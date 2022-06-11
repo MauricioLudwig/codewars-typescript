31a5e6c9395084817deaa48dcb35146f0105e9f0
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2022-06-12 00:09:51 +0200

    add vowels to alphabet utility

--- @@utils/alphabet.ts
+++ @@utils/alphabet.ts
@@ -2,6 +2,10 @@ export const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 
 export const alphabetArr = alphabet.split('');
 
+export const vowels = 'aeiouy';
+
+export const vowelsArr = vowels.split('');
+
 export const alphabetMap = (offset = 0): Record<string, number> =>
   alphabetArr.reduce(
     (acc, curr, index) => ({

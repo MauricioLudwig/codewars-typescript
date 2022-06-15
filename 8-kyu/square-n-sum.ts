cf1b5011d80478a1965e59acc669f0241f11358b
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2022-06-15 23:00:19 +0200

    remove comments

--- 8-kyu/square-n-sum.ts
+++ 8-kyu/square-n-sum.ts
@@ -1,13 +1,2 @@
-/*
-
-Complete the square sum function so that it squares each number passed into it and then sums the results together.
-
-For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
-
-*/
-
-// map [] => []
-// reduce [] => [[]], {}, 1
-
 export const squareSum = (numbers: number[]): number =>
   numbers.reduce((acc, curr) => acc + Math.pow(curr, 2), 0);

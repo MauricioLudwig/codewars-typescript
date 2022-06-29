5e37da51c4d788cc3389cae6d39ee77648420e8d
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2022-06-29 03:27:40 +0200

    fix linting

--- 6-kyu/find-the-missing-letter.ts
+++ 6-kyu/find-the-missing-letter.ts
@@ -1,17 +1,22 @@
 import { alphabetArr } from '../@@utils/alphabet';
+import { assertIsDefined } from '../@@utils/assertions';
 
 export const findMissingLetter = (arr: string[]): string => {
   const arrLowerCase = arr.map((o) => o.toLowerCase());
-  const isUpperCase = /[A-Z]/.test(arr[0]!);
-  let index = alphabetArr.findIndex((o) => o === arrLowerCase[0]);
+  const [firstItem] = arr;
+  assertIsDefined(firstItem);
+  const isUpperCase = /[A-Z]/.test(firstItem);
+  const index = alphabetArr.findIndex((o) => o === arrLowerCase[0]);
 
   if (index === -1) {
     throw new Error('Index of alphabet letter was not found.');
   }
 
   for (let i = 0; i < arr.length; i++) {
-    const l1 = arrLowerCase[i]!;
-    const l2 = alphabetArr[index + i]!;
+    const l1 = arrLowerCase[i];
+    const l2 = alphabetArr[index + i];
+    assertIsDefined(l1);
+    assertIsDefined(l2);
     const isCorrectSeq = l1 === l2;
 
     if (!isCorrectSeq) {
